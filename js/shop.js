b45c5d558daacb301c63146b414e067007b5d8e8
maher-mme <essafi.mohamed.maher@gmail.com>
2026-04-23 16:04:27 +0200

    suprimation de pikachu et toad

--- js/shop.js
+++ js/shop.js
@@ -36,8 +36,7 @@ var SKINS_BOUTIQUE = [
   { id: 'pomni', nom: 'Pomni', fichier: 'skin/boutique/pomni.svg', prix: 400, rarete: 'epic' },
   { id: 'caine', nom: 'Caine', fichier: 'skin/boutique/Caine-immobile.svg', fichierMove: 'skin/boutique/Caine-d\u00e9placment.svg', prix: 900, rarete: 'legendaire', animated: true },
   { id: 'frilleu', nom: 'Frilleu', fichier: 'skin/boutique/frilleu.svg', prix: 100, rarete: 'commun' },
-  { id: 'creeper', nom: 'Creeper', fichier: 'skin/boutique/creeper.svg', prix: 250, rarete: 'rare' },
-  { id: 'toad', nom: 'Toad', fichier: 'skin/boutique/toad.svg', prix: 150, rarete: 'commun' }
+  { id: 'creeper', nom: 'Creeper', fichier: 'skin/boutique/creeper.svg', prix: 250, rarete: 'rare' }
 ];
 
 function getSkinsAchetes() {
@@ -283,8 +282,7 @@ var PETS_BOUTIQUE = [
   { id: 'chien-spatial', nom: 'Chien Spatial', idle: 'pets/chien_de_l\'espsace1.svg', walk1: 'pets/chien_de_l\'espsace2.svg', walk2: 'pets/chien_de_l\'espsace3.svg', prix: 50, rarete: 'typique' },
   { id: 'lapin', nom: 'Lapin', idle: 'pets/Rabbit-a.svg', walk1: 'pets/Rabbit-a.svg', walk2: 'pets/Rabbit-b.svg', prix: 100, rarete: 'commun' },
   { id: 'bubble', nom: 'Bubble', idle: 'pets/buble.svg', walk1: 'pets/buble.svg', walk2: 'pets/buble2.svg', prix: 150, rarete: 'rare' },
-  { id: 'dragon-feu', nom: 'Dragon de Feu', idle: 'pets/dragon_de_feu.gif', walk1: 'pets/dragon_de_feu.gif', walk2: 'pets/dragon_de_feu.gif', prix: 150, rarete: 'epic', isGif: true, sizePx: 80 },
-  { id: 'pikachu', nom: 'Pikachu', idle: 'pets/pikachu.svg', walk1: 'pets/pikachu.svg', walk2: 'pets/pikachu2.svg', prix: 100, rarete: 'commun', sizePx: 65 }
+  { id: 'dragon-feu', nom: 'Dragon de Feu', idle: 'pets/dragon_de_feu.gif', walk1: 'pets/dragon_de_feu.gif', walk2: 'pets/dragon_de_feu.gif', prix: 150, rarete: 'epic', isGif: true, sizePx: 80 }
 ];
 
 var petEquipe = localStorage.getItem('virusPet') || '';

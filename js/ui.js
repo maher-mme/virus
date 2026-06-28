a093db5d5ec5500a6fea206ace64faa1b1725f0d
maher-mme <essafi.mohamed.maher@gmail.com>
2026-06-28 18:15:48 +0200

    fix: scroll salon-main + cache CREER sur telephone <600px (v3.6.8)

--- js/ui.js
+++ js/ui.js
@@ -289,7 +289,7 @@ if ('serviceWorker' in navigator) {
 }
 
 // === DETECTION DE MISE A JOUR ===
-var CURRENT_VERSION = '3.6.7';
+var CURRENT_VERSION = '3.6.8';
 var _updateDismissed = false;
 var _updateForceTimer = null;
 

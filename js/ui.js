5b6fc99852e30e183e0deb95fa48f55707340d2c
maher-mme <essafi.mohamed.maher@gmail.com>
2026-07-01 16:08:40 +0200

    fix: remplace claud_logo.png (404) par placeholder CSS (v3.6.15)

--- js/ui.js
+++ js/ui.js
@@ -289,7 +289,7 @@ if ('serviceWorker' in navigator) {
 }
 
 // === DETECTION DE MISE A JOUR ===
-var CURRENT_VERSION = '3.6.14';
+var CURRENT_VERSION = '3.6.15';
 var _updateDismissed = false;
 var _updateForceTimer = null;
 
